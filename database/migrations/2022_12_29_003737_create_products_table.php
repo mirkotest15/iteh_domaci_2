35bf4a9de1493e981c53e6a8d58ce35af972b763
Mirko Nikic <4370557@gmail.com>
2022-12-29 01:45:45 +0100

    Added two create migrations

---
+++ database/migrations/2022_12_29_003737_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('color_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('name');
+            $table->integer('memory');
+            $table->integer('storage');
+            $table->decimal('display_diagonal');
+            $table->string('os');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
